f130c0f64000f6a65642d0cd29ac0319e0291ba1
Quintin Donnelly <quinndonnelly22@gmail.com>
2018-02-18 18:02:58 -0600

    Starting Structure (#1)
    * Added the basic setup files to start off the folder structure
    
    * Removed the user file that was used to test database. Added the travis yml
    
    * Removed the chrome virtual stuff from the travis build
    
    * The hours of work i put into those 1,394,095 lines of code will never be known to the world i am now removing them

---
+++ server.js
@@ -0,0 +1,53 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config');
+
+const env = process.env.NODE_ENV || 'development';
+
+// Create the server for the API to run on
+const app = express();
+
+// View engine configs
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Set up middleware for the API server
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+// Testing route to ensure server is up
+app.get('/', (req, res) => {
+  res.json({
+    'Welcome to the CSC 424 API': '',
+    'uptime': `${process.uptime()}`,
+  });
+});
+
+// error handler
+// no stacktraces leaked to user unless in development environment
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: (app.get('env') === 'development') ? err : {},
+  });
+});
+
+// Creating a section for dev configs and prod configs
+if (env === 'development') {
+  app.listen(config.devPort, () => {
+    console.log(`API server started at http://localhost:${config.devPort}`);
+  });
+} else {
+  app.listen(config.prodPort, () => {
+    console.log(`API server started at http://localhost:${config.prodPort}`);
+  });
+}
