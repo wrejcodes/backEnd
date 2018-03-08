3971c4352eaf4a7176d7a506d047770cc4483fd1
Quintin Donnelly <quinndonnelly22@gmail.com>
2018-03-07 22:12:49 -0600

    Ruby updates to include multiple output types. (#22)
    UML JSON and SQL are the current output format types
    they now output to files with endings matching the output type

--- utils/parse_csv.rb
+++ utils/parse_csv.rb
@@ -1,6 +1,12 @@
 require 'csv'
 
 filename = ARGV[0]
+output_type = ARGV[1]
+types = ['JSON', 'SQL', 'UML']
+
+join_string = ["\n", ',', "\n"]
+current_type = "SQL"
+types.each_with_index {|type, index| current_type = index if output_type == type}
 counter = 0
 
 headers = []
@@ -36,12 +42,19 @@ fields.each_with_index do |field, index|
 end
 
 fields.each_with_index do |field, index|
-  combined.push "#{headers[index]}:#{field}"
+  if output_type == 'JSON'
+    field = 'number' if field == 'integer' or field == 'float'
+    field = 'text' if field == 'string'
+    field = 'checkbox' if field == 'bool'
+  end
+  label = headers[index].split('_').each(&:capitalize!).join(' ')
+  format_strings =["{ type: '#{field}', label: '#{label}' name:'#{headers[index]}' },", "#{headers[index]}:#{field}", "+ #{headers[index]}:#{field}"]
+  combined.push format_strings[current_type]
 end
 
-combined_string = combined.join(",")
+combined_string = combined.join(join_string[current_type])
 
 
-File.open("#{File.dirname(filename)}/#{File.basename(filename, ".csv")}.txt", 'w') do |file|
+File.open("#{File.dirname(filename)}/#{File.basename(filename, ".csv")}.#{types[current_type].downcase}", 'w') do |file|
   file.write(combined_string)
 end
