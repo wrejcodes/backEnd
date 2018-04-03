2bac358877dd9566b2d66f7c4b3f7a5fd27926c1
wrejcodes <wrejcodes@gmail.com>
2018-04-02 22:02:34 -0500

    Ruby Script Seeddata (#29)
    Now I wrap strings in single quotes and we can generate seed data for the database with this script (#29)

--- utils/parse_csv.rb
+++ utils/parse_csv.rb
@@ -2,9 +2,9 @@ require 'csv'
 
 filename = ARGV[0]
 output_type = ARGV[1]
-types = ['JSON', 'SQL', 'UML', 'TESTDATA']
+types = ['JSON', 'SQL', 'UML', 'TESTDATA', 'SEEDDATA']
 
-join_string = ["\n", ',', "\n", ",\n"]
+join_string = ["\n", ',', "\n", ",\n", ",\n"]
 current_type = "SQL"
 types.each_with_index {|type, index| current_type = index if output_type == type}
 counter = 0
@@ -23,7 +23,7 @@ end
 
 CSV.foreach(filename) do |row|
   row_index = 0
-  if output_type == 'TESTDATA'
+  if output_type == 'TESTDATA' || output_type == 'SEEDDATA'
     row.each do |item|
       headers.push item if counter == 0
       if counter > 0
@@ -32,7 +32,7 @@ CSV.foreach(filename) do |row|
       end
     end
     counter += 1
-    break if counter > 5;
+    break if counter > 5 && output_type == 'TESTDATA';
   else
     row.each do |item|
       headers.push item if counter == 0
@@ -48,7 +48,7 @@ CSV.foreach(filename) do |row|
   end
 end
 
-if output_type != "TESTDATA"
+if output_type != "TESTDATA" && output_type != 'SEEDDATA'
   fields.each_with_index do |field, index|
     fields[index] = "string" if field == "null" 
   end
@@ -56,19 +56,25 @@ end
 
 fields.each_with_index do |field, index|
   if output_type == 'JSON'
-    field = 'number' if field == 'integer' or field == 'float'
+    field = 'number' if field == 'integer' || field == 'float'
     field = 'text' if field == 'string'
     field = 'checkbox' if field == 'bool'
   end
+  if output_type == 'TESTDATA' || output_type == 'SEEDDATA'
+    type = get_type(field)
+    if type == 'bool'
+      field = field[0] == 'T' ? 'true': 'false'
+    end
+  end
   label = headers[index % headers.size].split('_').each(&:capitalize!).join(' ')
-  format_strings =["{ type: '#{field}', label: '#{label}' name:'#{headers[index]}' },", "#{headers[index]}:#{field}", "+ #{headers[index]}:#{field}", "#{index % headers.size == 0 ? "\t{\n" : ""}\t\t#{headers[index % headers.size]}: #{field}#{index % headers.size == headers.size - 1 ? ",\n\t}" : ""}"]
+  format_strings =["{ type: '#{field}', label: '#{label}' name:'#{headers[index]}' },", "#{headers[index]}:#{field}", "+ #{headers[index]}:#{field}", "#{index % headers.size == 0 ? "\t{\n" : ""}\t\t#{headers[index % headers.size]}: #{type && type == 'string' ? "'" : ""}#{field}#{type && type == 'string' ? "'" : ""}#{index % headers.size == headers.size - 1 ? ",\n\t}" : ""}","#{index % headers.size == 0 ? "\t{\n" : ""}\t\t#{headers[index % headers.size]}: #{type && type == 'string' ? "'" : ""}#{field}#{type && type == 'string' ? "'" : ""}#{index % headers.size == headers.size - 1 ? ",\n\t}" : ""}"]
   combined.push format_strings[current_type]
 end
 
 combined_string = combined.join(join_string[current_type])
 
-combined_string += ",\n];" if output_type == "TESTDATA"
-combined_string = "[\n" + combined_string if output_type == "TESTDATA"
+combined_string += ",\n];" if output_type == "TESTDATA" || output_type == 'SEEDDATA'
+combined_string = "[\n" + combined_string if output_type == "TESTDATA" || output_type == 'SEEDDATA'
 
 File.open("#{File.dirname(filename)}/#{File.basename(filename, ".csv")}.#{types[current_type].downcase}", 'w') do |file|
   file.write(combined_string)
