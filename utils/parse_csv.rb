a1884061d36a42e77923c4bbc1ec5850ac8fcfdb
wrejcodes <wrejcodes@gmail.com>
2018-04-02 19:56:30 -0500

    Added capability to generate test data from csvs :wink: (#28)

--- utils/parse_csv.rb
+++ utils/parse_csv.rb
@@ -2,9 +2,9 @@ require 'csv'
 
 filename = ARGV[0]
 output_type = ARGV[1]
-types = ['JSON', 'SQL', 'UML']
+types = ['JSON', 'SQL', 'UML', 'TESTDATA']
 
-join_string = ["\n", ',', "\n"]
+join_string = ["\n", ',', "\n", ",\n"]
 current_type = "SQL"
 types.each_with_index {|type, index| current_type = index if output_type == type}
 counter = 0
@@ -23,22 +23,35 @@ end
 
 CSV.foreach(filename) do |row|
   row_index = 0
-  row.each do |item|
-    headers.push item if counter == 0
-    type = get_type(item)
-    fields.push type if counter == 1
-    if counter > 1
-      type = get_type(item) if fields[row_index] == "null"
-      fields[row_index] = type if type != "null"
+  if output_type == 'TESTDATA'
+    row.each do |item|
+      headers.push item if counter == 0
+      if counter > 0
+        item = 'null' if item == '\N'
+        fields.push item
+      end
     end
-    row_index += 1
+    counter += 1
+    break if counter > 5;
+  else
+    row.each do |item|
+      headers.push item if counter == 0
+      type = get_type(item)
+      fields.push type if counter == 1
+      if counter > 1
+        type = get_type(item) if fields[row_index] == "null"
+        fields[row_index] = type if type != "null"
+      end
+      row_index += 1
+    end
+    counter += 1
   end
-  counter += 1
-
 end
 
-fields.each_with_index do |field, index|
-  fields[index] = "string" if field == "null"
+if output_type != "TESTDATA"
+  fields.each_with_index do |field, index|
+    fields[index] = "string" if field == "null" 
+  end
 end
 
 fields.each_with_index do |field, index|
@@ -47,13 +60,15 @@ fields.each_with_index do |field, index|
     field = 'text' if field == 'string'
     field = 'checkbox' if field == 'bool'
   end
-  label = headers[index].split('_').each(&:capitalize!).join(' ')
-  format_strings =["{ type: '#{field}', label: '#{label}' name:'#{headers[index]}' },", "#{headers[index]}:#{field}", "+ #{headers[index]}:#{field}"]
+  label = headers[index % headers.size].split('_').each(&:capitalize!).join(' ')
+  format_strings =["{ type: '#{field}', label: '#{label}' name:'#{headers[index]}' },", "#{headers[index]}:#{field}", "+ #{headers[index]}:#{field}", "#{index % headers.size == 0 ? "\t{\n" : ""}\t\t#{headers[index % headers.size]}: #{field}#{index % headers.size == headers.size - 1 ? ",\n\t}" : ""}"]
   combined.push format_strings[current_type]
 end
 
 combined_string = combined.join(join_string[current_type])
 
+combined_string += ",\n];" if output_type == "TESTDATA"
+combined_string = "[\n" + combined_string if output_type == "TESTDATA"
 
 File.open("#{File.dirname(filename)}/#{File.basename(filename, ".csv")}.#{types[current_type].downcase}", 'w') do |file|
   file.write(combined_string)
